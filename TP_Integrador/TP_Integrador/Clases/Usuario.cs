312e360d0b17014051623134dff1f59f38732608
JuanManuelSakkal <jmsakkal@hotmail.com>
2018-05-08 11:36:33 -0300

    Primer commit, clase Usuario creada

---
+++ TP_Integrador/TP_Integrador/Clases/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP_Integrador.Clases
+{
+    public class Usuario
+    {
+        public int idUsuario{ get; set; }
+        public string nombreUsuario { get; set; }
+        public string password { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string domicilio { get; set; }
+        public DateTime fechaDeAlta { get; set; }
+    }
+}
