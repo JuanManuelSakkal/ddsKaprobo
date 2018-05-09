9c9a85592532523c7f18dac0bb97db7b059b1242
sebikap <sebikap@hotmail.com>
2018-05-09 00:06:35 -0300

    Comienzo de importadores

---
+++ TP_Integrador/TP_Integrador/Helpers/UsuarioImporter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TP_Integrador.Clases;
+
+namespace TP_Integrador.Helpers
+{
+    public class JsonImporter
+    {
+        string path;
+        HttpServerUtility server = HttpContext.Current.Server;
+        public JsonImporter(string locationString)
+        {
+            path = server.MapPath(@locationString);
+        }
+
+        public Dispositivo ImportarDispositivo()
+        {
+            StreamWriter wr = new StreamWriter(path, true);
+            return new Dispositivo();
+        }
+
+        public Usuario ImportarUsuario()
+        {
+            StreamWriter wr = new StreamWriter(path, true);
+            return new Usuario();
+        }
+    }
+}
